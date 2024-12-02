c9da2ce83d31636aef609490eba2023314be068d
DanielDaly33133 <ddaly33133@gmail.com>
2024-12-01 20:11:42 -0500

    v0.2 Commit

--- #AutoCountry CarFinder App.py
+++ #AutoCountry CarFinder App.py
@@ -1,15 +1,18 @@
 #AutoCountry CarFinder App
 #Approved Vehilcle List
+
 allowedVehiclesList = [ 'Ford F-150', 'Chevrolet Silverado', 'Tesla CyberTruck', 'Toyota Tundra', 'Nissan Titan' ]
+
 
 #Menu w/ Options
 def menu():
     print('********************************')
-    print('AutoCountry Vehicle Finder v0.1')
+    print('AutoCountry Vehicle Finder v0.2')
     print('********************************')
     print("Please Enter the following number below from the following menu: ")
     print("1. Show all Authorized Vehicles")
-    print("2. Exit")
+    print("2. Search for Authorized Vehicle")
+    print("3. Exit")
 
 menu()
 option = int(input("Please Enter the following number below from the following menu: "))
@@ -17,14 +20,28 @@ option = int(input("Please Enter the following number below from the following m
 # Menu Options Parameters
 
 #While loop
-while option != 2:    
-    if option == 1:  
+while option != 3:    
+    if option == 1: 
         print(allowedVehiclesList)
-    else:
-        print('That is not a valid option, please try again.')
-    print()
-    menu()
-    option = int(input("Please Enter the following number below from the following menu: "))
+    if option == 2:
+        response1 = (input('Please Enter the full Vehicle name: ')) 
+        for a in allowedVehiclesList:
+            if a == response1:
+                print(response1 + ' is an authorized vehicle')
+                print('********************************')
+                print('AutoCountry Vehicle Finder v0.2')
+                print('********************************')
+                print("Please Enter the following number below from the following menu: ")
+                print("1. Show all Authorized Vehicles")
+                print("2. Search for Authorized Vehicle")
+                print("3. Exit")
+                #break or else it marks true response as invalid
+                break
+                print(menu())
+                option = int(input("Please Enter the following number below from the following menu: "))
+        else:
+            print( response1 + ' is not an authorized vehicle, if you received this in error please check the spelling and try again')
+            print(menu())
 
 
 print('Thank you for using the AutoCountry Vehicle Finder, good-bye!')
