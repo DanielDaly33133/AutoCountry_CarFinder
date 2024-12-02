cfd69fa9b387883b2b8f587e78665ab0ddd92ae9
DanielDaly33133 <ddaly33133@gmail.com>
2024-12-01 22:33:38 -0500

    v0.3 Commit

--- #AutoCountry CarFinder Appv0.3.py
+++ #AutoCountry CarFinder Appv0.3.py
@@ -1,8 +1,8 @@
-#AutoCountry CarFinder App v0.2
+#AutoCountry CarFinder App v0.3
 #Approved Vehilcle List
 
-allowedVehiclesList = [ 'Ford F-150', 'Chevrolet Silverado', 'Tesla CyberTruck', 'Toyota Tundra', 'Nissan Titan' ]
 
+allowedVehiclesList = [ 'Ford F-150', 'Chevrolet Silverado', 'Tesla CyberTruck', 'Toyota Tundra', 'Nissan Titan' ]
 
 #Menu w/ Options
 def menu():
@@ -12,8 +12,10 @@ def menu():
     print("Please Enter the following number below from the following menu: ")
     print("1. Show all Authorized Vehicles")
     print("2. Search for Authorized Vehicle")
-    print("3. ADD Authorized Vehicle")
+    print("3. ADD authorized vehicle")
     print("4. Exit")
+
+    
 
 menu()
 option = int(input("Please Enter the following number below from the following menu: "))
@@ -23,32 +25,41 @@ option = int(input("Please Enter the following number below from the following m
 #While loop
 while option != 4:    
     if option == 1: 
+        print('The AutoCountry sales manager has authorized the purchase and selling of the following vehicles: ')
         print(allowedVehiclesList)
+        print(menu())
+        option = int(input("Please Enter the following number below from the following menu: "))
     if option == 2:
         response1 = (input('Please Enter the full Vehicle name: ')) 
         for a in allowedVehiclesList:
-            if a == response1:aaa
-            print(response1 + ' is an authorized vehicle')
-            print('********************************')
-            print('AutoCountry Vehicle Finder v0.3')
-            print('********************************')
-            print("Please Enter the following number below from the following menu: ")
-            print("1. Show all Authorized Vehicles")
-            print("2. Search for Authorized Vehicle")
-            print("3. Exit")
-            #break or else it marks true response as invalid
-            break
-            print(menu())
-            option = int(input("Please Enter the following number below from the following menu: "))
+            if a == response1:
+                print(response1 + ' is an authorized vehicle')
+                print('********************************')
+                print('AutoCountry Vehicle Finder v0.2')
+                print('********************************')
+                print("Please Enter the following number below from the following menu: ")
+                print("1. Show all Authorized Vehicles")
+                print("2. Search for Authorized Vehicle")
+                print("3. Exit")
+                #break or else it marks true response as invalid
+                break
+                print(menu())
+                option = int(input("Please Enter the following number below from the following menu: "))
+    
     if option == 3:
-        response2 = (input('Please enter the full vehicle name you would like to add: '))
+        print(menu())
+        response2 = input('Please Enter the FULL NAME of the vehicle you would like to ADD: ')
         allowedVehiclesList.append(response2)
-        print('You have added ' + (response2) +' as an authorized vehicle')
+        print("You have added " + (response2) + " as an authorized vehicle")
         print(menu())
         option = int(input("Please Enter the following number below from the following menu: "))
-else:
-    print( response1 + ' is not an authorized vehicle, if you received this in error please check the spelling and try again')
-    print(menu())
+
+    else:
+        print( response1 + ' is not an authorized vehicle, if you received this in error please check the spelling and try again')
+        break
+            
+print(menu())
+option = int(input("Please Enter the following number below from the following menu: "))   
 
 
 print('Thank you for using the AutoCountry Vehicle Finder, good-bye!')
